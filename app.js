ecb4da05f2f09afff22fe6dad0d8bdf98d1f1f02
Ian J <ianbennettjohnson@gmail.com>
2020-04-06 15:15:29 -0700

    Created all the lib and most of the app.js

--- app.js
+++ app.js
@@ -1,16 +1,153 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const Employee = require("./lib/Employee");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
+
 const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 ​
 const render = require("./lib/htmlRenderer");
-​
-​
+​const team = [];
+const questionStart = [{
+    type: "list",
+    name: "jobTitle",
+    massage: "What is the title of the employee you are adding?",
+    choices: ["Engineer", "Manager", "Intern", "Employee", "Finished"]
+}];
+
+//Engineer Question
+const engiQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is this lunatic's name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is this lunatic's ID",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is this lunatic's email?" 
+    },
+    {
+        type: "input",
+        name: "gitHub",
+        message: "What is this lunatic's gitHub Account?"
+    }
+];
+
+//Manager Questions
+const manaQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is this so-called Manager's name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is this crazy person's ID",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is this lunatic's email?"
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "Where is this maniacss office?"
+    }
+];
+
+//Intern Question
+const internQuestions =[
+    {
+        type: "input",
+        name: "name",
+        message: "What is this intern's name?",
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is this suspect's id?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is this poor soul's email?",
+    },
+    {
+        type: "input",
+        name: "school",
+        message: "What school is allowing this idiot to call it home?",
+    }
+];
+
+//Employee Question
+const empQuestions =[
+    {
+        type: "input",
+        name: "name",
+        message: "What is this intern's name?",
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is this suspect's id?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is this poor soul's email?",
+    },
+];
+
+
+function add() {
+    inquirer.prompt(questionStart).then(answers => {
+        if(answers.jobTitle === "Engineer"){
+            inquirer.prompt(engiQuestions).then(answer => {
+                team.push(new Manager(answer.name, answer.id, asnwer.email, answer.officeNumber));
+                add();
+            });
+        } else if(answers.jobTitle === "Manager"){
+            inquirer.prompt(manaQuestions).then(answer => {
+                team.push(new Engineer(answer.name, answer.id, answer.email, answer.gitHub));
+                add();
+            });
+        } else if(answers.jobTitle === "Intern"){
+            inquirer.prompt(internQuestions).then(answer => {
+                team.push(new Intern(answer.name, answer.id, answer.email, answer.school));
+                add();
+            });
+        } else if(answers.jobTitle === "Employee"){
+            inquirer.prompt(empQuestions).then(answer => {
+                team.push(new Employee(answer.name, answer.id, answer.email));
+                add();
+            });
+        }else{
+            const html = render(team);
+            fs.writeFileSync(outputPath, html, err => {
+                if(err){
+                    throw err;
+                }
+            })
+        };
+    });
+};
+
+add();
+
+
+
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 ​
